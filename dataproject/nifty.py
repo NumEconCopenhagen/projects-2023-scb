662abb85707442755a5e1ad222787002480f9105
CarlBenjaminSimpson <carlsimpson1@gmail.com>
2023-04-10 12:25:42 +0200

    ingenting

--- dataproject/nifty.py
+++ dataproject/nifty.py
@@ -4,24 +4,26 @@ import numpy as np
 import os
 os.getcwd()
 
-
 #%%
 input_dir = os.path.join(os.getcwd(),'data')
-
-#%%
-nifty50 = pd.read_csv(os.path.join(input_dir,'OMXS30.csv'), sep=',')
-
-# %%
+nifty50 = pd.read_csv(os.path.join(input_dir,'NSEI2.csv'), sep=',')
 nifty50.head()
 
-#%%
-nifty50['log_daily_return'] = np.log(nifty50['Adj Close']) - np.log(nifty50['Adj Close'].shift(1))
+rename_dict = {} #initiate dict
+for i in nifty50.columns: #loop over names in columns
+    # creates dict for renaming variables with no upper case letters
+    # nor any spaces
+    rename_dict[i] = i.lower().replace(' ', '_') 
+nifty50 = nifty50.rename(columns=rename_dict)
 
 #%%
-nifty50['daily_return'] = (nifty50['Adj Close']-nifty50['Adj Close'].shift(1))/nifty50['Adj Close'].shift(1)
+nifty50['log_daily_return'] = np.log(nifty50['adj_close']) - np.log(nifty50['adj_close'].shift(1))
+
+#%%
+nifty50['daily_return'] = (nifty50['adj_close']-nifty50['adj_close'].shift(1))/nifty50['adj_close'].shift(1)
 
 # %%
-nifty50.plot('Date', 'daily_return')
+nifty50.plot('date', 'daily_return')
 
 # %%
 nifty50.head()
@@ -30,34 +32,59 @@ nifty50.head()
 nifty50['dif'] = nifty50.daily_return - nifty50.log_daily_return
 
 # %%
-nifty50.plot('Date', 'dif') 
-
+nifty50.plot('date', 'dif')
 
 #%%
-nifty50['dato'] = pd.to_datetime(nifty50['Date'])
-
-nifty50.plot('dato', 'Volume')
+nifty50.plot('date', 'volume')
 # %%
-nifty50_volume = nifty50.loc[nifty50.Volume>0, ['Volume', 'dato']]
+nifty50_volume = nifty50.loc[nifty50.volume>0, ['volume', 'date']]
 # %%
-nifty50_volume.plot('dato', 'Volume')
+nifty50_volume.plot('date', 'volume')
 
 # %%
-nifty50
-# %%
+easter_dates = pd.read_pickle(os.path.join(input_dir, 'dates.pkl'))
+easter_dates.info()
+
+#%%
+nifty50['date'] = pd.to_datetime(nifty50['date'])
+easter_dates['date'] = pd.to_datetime(easter_dates['date'])
+
+#%%
+nifty50_easter = pd.merge(nifty50, easter_dates, how='left', on='date')
+
+#%%
+nifty50_easter['easter_week'] = nifty50_easter['easter_week'].replace(np.nan, 0)
+nifty50_easter['easter_week'].value_counts()
+
+#%%
+mean_return_easter_holiday = nifty50_easter.loc[nifty50_easter.easter_week==1, 'daily_return'].mean()
+mean_return = nifty50_easter.loc[nifty50_easter.easter_week!=1, 'daily_return'].mean()
+difference = mean_return_easter_holiday - mean_return
+
+print(difference)
+nifty50_easter.loc[nifty50_easter.easter_week==1].plot('date', 'daily_return', kind='kde')
+nifty50_easter.loc[nifty50_easter.easter_week!=1].plot('date', 'daily_return', kind='kde')
+
+#%%
+
+
+
+# %% import using yfinance modulr
 import pandas as pd
 from pandas_datareader import data as pdr
 import yfinance as yfin
-
-
+import datetime as dt
 yfin.pdr_override()
 
-spy = pdr.get_data_yahoo('SPY', start='2022-10-24', end='2022-12-23')
-
-
+#%%
 start = dt.datetime(2010, 1, 1)
-
 end = dt.datetime.today()
+nifty50 = pdr.get_data_yahoo('NFTY', start, end)
 
+# %%
+nifty50 = pdr.get_data_yahoo('NFTY', start, end)
 
-actions = pdr.get_data_yahoo('GOOG', start, end)
+# %%
+nifty50
+
+# %%
