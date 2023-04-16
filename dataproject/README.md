af15d7302c3947285e9639f7b0bd322a600a65af
CarlBenjaminSimpson <carlsimpson1@gmail.com>
2023-04-16 23:34:51 +0200

    bug fixes

--- dataproject/README.md
+++ dataproject/README.md
@@ -1,13 +1,13 @@
 # Data analysis project
 
-Our project is titled **PROJECT TITLE** and is about EXPLAIN.
+Our project is titled **Stock Returns during Easter** and is about stock returns during easter.
 
 The **results** of the project can be seen from running [dataproject.ipynb](dataproject.ipynb).
 
 We apply the **following datasets**:
 
-1. dataX.csv (*source*) 
-1. dataY.csv (*source*)
+1. NSEI2.csv (*yahoo finance*) 
+1. OMXS30.csv (*yahoo finance*)
 
 **Dependencies:** Apart from a standard Anaconda Python 3 installation, the project requires the following installations:
 
--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 from matplotlib import pyplot as plt
 import ipywidgets as widgets
+from IPython.display import display, clear_output
 
 def read_yahoo(input_dir, filename='file.csv'):
     """ Read csv file from Yahoo finance to df\n
@@ -31,7 +32,7 @@ def plot_stock_interactive(merge_final):
         y_max = 0.075 # Choose value y axis maximum
         y_min = -0.11 # Choose value y axis minimum
         if start_date <= end_date and stock_index!='both': # assert that dates chose make sense
-            fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(8,6)) # initiate plot
+            fig, ax = plt.subplot(nrows=1, ncols=1, figsize=(8,6)) # initiate plot
             I = (merge_final.date>=start_date)&(merge_final.date<=end_date) # dataframe slice condition
             x = merge_final.loc[I, 'date'] # x values
             y = merge_final.loc[I, f'daily_return_{stock_index}'] # y-values
@@ -46,6 +47,7 @@ def plot_stock_interactive(merge_final):
                        alpha=0.3,
                        linewidth=6, label="Days during easter")
             ax.legend(loc="lower center")
+            clear_output(wait=True)
         elif stock_index == 'both' and start_date <= end_date:
             fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(8,6))
             I = (merge_final.date>=start_date)&(merge_final.date<=end_date)
@@ -63,11 +65,10 @@ def plot_stock_interactive(merge_final):
                        alpha=0.3, linewidth=6, label="Days during easter")
             ax.set_title(f"Daily returns for 'OMXS', 'NIFTY FIFTY'")
             ax.legend(loc="lower center")
+            clear_output(wait=True)
         else: 
              print("Hello mate, please choose a valid range of dates")
         
-    return widgets.interact(plot_stock_index, stock_index=['both', 'omxs', 'nifty'],
+    widgets.interact(plot_stock_index, stock_index=['both', 'omxs', 'nifty'],
                               start_date = widgets.SelectionSlider(value= pd.to_datetime("2016-05-03 00:00:00"), options=merge_final.date, step=0.04),
                                 end_date = widgets.SelectionSlider(value=pd.to_datetime("2016-06-09 00:00:00"), options=merge_final.date), step=1);
-
-# %%
