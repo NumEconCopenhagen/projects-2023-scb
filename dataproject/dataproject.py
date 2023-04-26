d86414dc2d5b7d4ae9244ee5fffe23f5786f145c
CarlBenjaminSimpson <carlsimson1@gmail.com>
2023-04-26 17:56:55 +0200

    interactive plot displaying too many times

--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 from matplotlib import pyplot as plt
-from ipywidgets import Output, IntSlider, VBox
+from ipywidgets import Output, SelectionSlider, VBox
 import ipywidgets as widgets
 from IPython.display import display, clear_output
 
@@ -26,17 +26,18 @@ def plot_stock_interactive(merge_final):
     Args: merge_final = cleaned datframe
 
     Returns interactive plot for cleaned dataframe
-
+    
     """
     out = Output()
     with out:
-        clear_output()
         def plot_stock_index(stock_index, start_date=min(merge_final.date), end_date=max(merge_final.date)):
+            clear_output()
             vertical_lines = merge_final.loc[merge_final.easter_week==1, 'date'].to_numpy()
             y_max = 0.075 # Choose value y axis maximum
             y_min = -0.11 # Choose value y axis minimum
-            if start_date <= end_date and stock_index=='omxs' or stock_index=='nifty': # assert that dates chose make sense
+            if start_date <= end_date and stock_index=='omxs': #or stock_index=='nifty': # assert that dates chose make sense
                 fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(5,5)) # initiate plot
+                ax.clear()
                 I = (merge_final.date>=start_date)&(merge_final.date<=end_date) # dataframe slice condition
                 x = merge_final.loc[I, 'date'] # x values
                 y = merge_final.loc[I, f'daily_return_{stock_index}'] # y-values
@@ -51,8 +52,10 @@ def plot_stock_interactive(merge_final):
                            alpha=0.3,
                            linewidth=6, label="Days during easter")
                 ax.legend(loc="lower center")
+                plt.show()
             elif stock_index == 'both' and start_date <= end_date:
                 fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(5,5))
+                ax.clear()
                 I = (merge_final.date>=start_date)&(merge_final.date<=end_date)
                 for i in ['omxs', 'nifty']:
                     x = merge_final.loc[I, 'date']
@@ -72,10 +75,11 @@ def plot_stock_interactive(merge_final):
             else: 
                  print("Hello mate, please choose a valid range of dates")
         
-    widgets.interact(plot_stock_index, stock_index=['both', 'omxs', 'nifty'],
-                              start_date = widgets.SelectionSlider(value= pd.to_datetime("2016-05-03 00:00:00"), options=merge_final.date, step=0.04),
-                                end_date = widgets.SelectionSlider(value=pd.to_datetime("2016-06-09 00:00:00"), options=merge_final.date), step=1);
-    return display(VBox([out]))
+    out = widgets.interact(plot_stock_index, stock_index=['omxs', "nifty"], #start_date=merge_final.date, end_date=merge_final.date)#,
+                              start_date = SelectionSlider(value= pd.to_datetime("2016-05-03 00:00:00"), options=merge_final.date, step=1),
+                                end_date = SelectionSlider(value=pd.to_datetime("2016-06-09 00:00:00"), options=merge_final.date, step=1));
+    
+    return  display(out)
 
 
 #from ipywidgets import Output, IntSlider, VBox
