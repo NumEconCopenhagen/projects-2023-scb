b109b2e67e9c27eeda5853c698f235d04023dfa5
lrx992 <lrx992@alumni.ku.dk>
2023-05-02 17:00:04 +0200

    ja

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -1,22 +1,77 @@
 from scipy import optimize
+import numpy as np
+import time
+from types import SimpleNamespace
 
-def solve_ss(alpha, c):
-    """ Example function. Solve for steady state k. 
+class Solow():
 
-    Args:
-        c (float): costs
-        alpha (float): parameter
+    def __init__(self, do_print = True):
+        """ define the model """
 
-    Returns:
-        result (RootResults): the solution represented as a RootResults object.
+        if do_print: print('initialising the model'):
 
-    """ 
+        self.par = SimpleNamespace()
+        self.sim = SimpleNamespace()
+
+        if do_print: print('calling .setup()')
+        self.setup()
+
+
+    def setup(self):
+
+        par = self.par
+
+        par.alpha = 0.3
+        par.phi = 0.3
+        par.n = 0    
+        par.g = 0
+        par.production_function = 'cobb-douglas'
+        par.simT = 50
+        par.K_init = 0.75
+        par.H_init = 0.5
+        par.L_init = 1
+
     
-    # a. Objective function, depends on k (endogenous) and c (exogenous).
-    f = lambda k: k**alpha - c
-    obj = lambda kss: kss - f(kss)
 
-    #. b. call root finder to find kss.
-    result = optimize.root_scalar(obj,bracket=[0.1,100],method='bisect')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def solve_ss(alpha, c):
+#     """ Example function. Solve for steady state k. 
+
+#     Args:
+#         c (float): costs
+#         alpha (float): parameter
+
+#     Returns:
+#         result (RootResults): the solution represented as a RootResults object.
+
+#     """ 
     
-    return result
+#     # a. Objective function, depends on k (endogenous) and c (exogenous).
+#     f = lambda k: k**alpha - c
+#     obj = lambda kss: kss - f(kss)
+
+#     #. b. call root finder to find kss.
+#     result = optimize.root_scalar(obj,bracket=[0.1,100],method='bisect')
+    
+#     return result
