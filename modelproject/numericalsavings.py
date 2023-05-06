3bd32bd6f75dae31095ecdda4d341db2a10577d2
CarlBenjaminSimpson <carlsimpson1@gmail.com>
2023-05-06 15:55:47 +0200

    discrete solution

--- modelproject/numericalsavings.py
+++ modelproject/numericalsavings.py
@@ -1,6 +1,8 @@
-# %%
+
+#%%
 from scipy import optimize
 import numpy as np
+import time
 from types import SimpleNamespace
 
 class Solow():
@@ -9,9 +11,8 @@ class Solow():
         """ define the model """
 
         if do_print: print('initialising the model')
-        self.sol = SimpleNamespace()
-        self.par = SimpleNamespace()
-        self.sim = SimpleNamespace()
+
+        par = self.par = SimpleNamespace()
 
         if do_print: print('calling .setup()')
         self.setup()
@@ -20,25 +21,106 @@ class Solow():
     def setup(self):
 
         par = self.par
-        sol = self.sol
 
-        par.delta = 0.05
-        par.alpha = 0.3
-        par.phi = 0.2
-        par.n = 0.01
-        par.g = 0.01
         par.production_function = 'cobb-douglas'
-        par.simT = 50
+        par.alpha = 0.3
+        par.phi = 0.3
+
+        par.n = 0.01    
+        par.g = 0.02
+        par.delta = 0.05
+        
+        par.A_init = 1
         par.K_init = 1
         par.H_init = 1
         par.L_init = 1
-        par.A = 1
+        par.Y_init = 1
 
-        par.s_K = 0.01
-        par.s_H = 0.01
+        par.simT = 200
 
-        sol.y = 0
-        sol.con_ss = 0
+
+
+    
+    def find_steady_state(self, sK=0.07, sH=0.12, tol=1e-6):
+
+        par = self.par
+
+        A = np.empty(par.simT)
+        K = np.empty(par.simT)
+        H = np.empty(par.simT)
+        L = np.empty(par.simT)
+        Y = np.empty(par.simT)
+        y_tilde = np.empty(par.simT)
+        k_tilde = np.empty(par.simT)
+        h_tilde = np.empty(par.simT) 
+
+        for i,j in zip([A,K,H,L,Y], [par.A_init, par.K_init,par.H_init,par.L_init,par.Y_init]):
+            i[0] = j
+        
+        t = 1
+        while t < par.simT:
+       
+            A[t] = A[t-1]*(1+par.g)     
+            L[t] = L[t-1]*(1+par.n) 
+
+            H[t] = Y[t-1]*sH + (1-par.delta)*H[t-1]
+            K[t] = Y[t-1]*sK + (1-par.delta)*H[t-1]
+
+            if par.production_function == 'cobb-douglas':
+                Y[t] = (K[t]**par.alpha)*(H[t]**par.phi)*(A[t]*L[t])**(1-par.alpha-par.phi)
+            else:
+                Y[t] = np.nan
+            
+            y_tilde[t] = Y[t]/(A[t]*L[t])
+            k_tilde[t] = K[t]/(A[t]*L[t])
+            h_tilde[t] = H[t]/(A[t]*L[t])
+
+            if (k_tilde[t]-k_tilde[t-1] < tol) and (h_tilde[t]-h_tilde[t-1] < tol):
+                print("we are breaking")
+                break
+
+            t += 1
+        print(t)
+        return Y[t]
+
+
+    def cons_t(self, sK=0.07, sH=0.012):
+             consumption = (1-sK-sH)*self.find_steady_state(sK=sK, sH=sH, tol=1e-6)
+             return consumption
+
+    def find_opt_s(self):
+
+        opt = SimpleNamespace()
+
+        s_K = np.linspace(1e-8, 1, 10)
+        s_H = np.linspace(1e-8, 1, 10)
+
+        x = np.linspace(0,1,10)
+        s_K, s_H = np.meshgrid(x,x) # all combinations
+    
+        s_K = s_K.ravel() # vector
+        s_H = s_H.ravel()
+
+    
+        cons = self.cons_t(sk=s_K, sH=s_H)
+        # c. set to minus infinity if constraint is broken
+        I = (s_K+s_H > 1) # | is "or"
+        cons[I] = -np.inf
+    
+        # d. find maximizing argument
+        j = np.argmax(x)
+        
+        opt.s_H = s_H[j]
+        opt.s_K = s_K[j]
+       
+        # b. calculate utility
+
+        return opt
+    
+
+
+
+
 
     def prod_yss(self, x):
         par = self.par
@@ -54,7 +136,6 @@ class Solow():
         y = sol.y 
         return y
     
-
     #%%
     def cons_ss(self, x):
             par = self.par
@@ -83,4 +164,3 @@ class Solow():
             return gg
         else: 
             print("Ikke analytisk")
- 
